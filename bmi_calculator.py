e2402689e3b50f5e9c9655df1820c2d5e041f8bb
rabi3e <65637491+rabi3e@users.noreply.github.com>
2022-11-07 22:26:15 +0100

    bmi calculator

---
+++ bmi_calculator.py
@@ -0,0 +1,31 @@
+try:
+    # Taking user input
+    Height = float(input("Please enter your height in CM: "))
+    Weight = float(input("Please enter your weight in KG: "))
+except ValueError:
+    # Validation error.
+    print("Please provide valid input.")
+    # Return to the start of the loop.
+else:
+    if Height <= 0 or Weight <= 0:
+        print("Your input must not be zero or less.")
+    else:
+        # Calculate BMI
+        hm= (Height/100) * (Height/100)
+        BMIIndex = round(Weight / hm)
+
+        # Print the output
+        print("Your Body Mass Index is: ", BMIIndex)
+
+        if BMIIndex < 18.5:
+            print("Underweight.")
+        elif BMIIndex >= 18.5 and  BMIIndex<= 25:
+            print("Normal.")
+        elif BMIIndex > 25 and  BMIIndex<= 30:
+            print("Overweight")
+        elif BMIIndex > 30 and  BMIIndex<= 35:
+            print("Moderate obesity")
+        elif BMIIndex > 35 and  BMIIndex<= 40:
+            print("Severe obesity")
+        elif BMIIndex < 40:
+            print("Morbid obesity.")
